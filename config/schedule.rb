a33c535b3f7d3f2e6a4991bc59a45404e6c3b869
Ewout Quax <ewout.quax@quicknet.nl>
2016-08-02 10:53:51 +0200

    Runner lives in bin/rails
    The directory script/rails is old, but this schedule.rb is copied from
    an old rails-project.

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,4 @@
-job_type :runner,  "cd :path && RAILS_ENV=:environment bundle exec script/rails runner ':task' :output"
+job_type :runner, "cd :path && RAILS_ENV=:environment bundle exec bin/rails runner ':task' :output"
 
 every 30.minutes do
   runner 'Workers::WeatherMeasurementsCollector.invoke'
