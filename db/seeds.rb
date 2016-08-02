5f707f4a6de5e76c4e607b9b45d42fb582d4a935
Ewout Quax <ewout.quax@quicknet.nl>
2016-08-02 10:54:17 +0200

    New seeds-file, with data from production

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,8 @@
 Measurement.delete_all
-Measurement.create(
-  [measured_at: Time.parse('19:30') - 8.days, temperature: 16.3],
-  [measured_at: Time.parse('10:00') - 1.days, temperature: 27.8],
-  [measured_at: Time.parse('11:30'),          temperature: 5.1],
-  [measured_at: Time.parse('17:30'),          temperature: 6.2],
-  [measured_at: Time.parse('18:00'),          temperature: 8.9],
-  [measured_at: Time.parse('20:00'),          temperature: 4.3]
-)
+Measurement.create!([
+  {measured_at: "2016-07-30 18:53:48", temperature: "20.9", pressure: 101857, humidity: "52.0"},
+  {measured_at: "2016-08-01 19:01:51", temperature: "20.8", pressure: 101858, humidity: "52.0"},
+  {measured_at: "2016-07-31 19:34:06", temperature: "20.5", pressure: 101864, humidity: "52.0"},
+  {measured_at: "2016-08-01 19:35:36", temperature: "20.3", pressure: 101859, humidity: "52.8"},
+  {measured_at: "2016-08-01 20:02:17", temperature: "20.7", pressure: 101865, humidity: "53.0"}
+])
